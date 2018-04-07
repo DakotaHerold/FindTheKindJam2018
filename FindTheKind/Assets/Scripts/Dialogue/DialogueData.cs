73250fd747a9cea11b81ca51b347ce226ad49d88
Dakota Herold <dakota.herold@yahoo.com>
2018-04-06 23:50:00 -0400

    Added auto-type on text. Started working on dialogue branching.

---
+++ FindTheKind/Assets/Scripts/Dialogue/DialogueData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Dialogue/Dialogue Data", order = 1)]
+public class DialogueData : ScriptableObject {
+    struct DialoguePiece
+    {
+    }
+
+    public List<string> playerLines;
+    
+}
