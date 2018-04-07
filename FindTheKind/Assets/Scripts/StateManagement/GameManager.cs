b5c0720e75cc53d2fdaf72a77a0b2b4a5a8329e1
Dakota Herold <dakota.herold@yahoo.com>
2018-04-07 16:49:05 -0400

    Added Game Manager.

---
+++ FindTheKind/Assets/Scripts/StateManagement/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+    enum GAME_STATE
+    {
+        Title, 
+        Running, 
+        Talking, 
+        GameOver
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+
+    }
+
+    public void EndGame()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    #region Gameplay Functions
+
+    public void TriggerDialogue(DialogueData characterData)
+    {
+
+    }
+
+    public void ResumeScrolling()
+    {
+        
+    }
+    #endregion
+}
