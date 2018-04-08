23267c1bfeab86c62444a7721a1f48018ad21758
Justin Dennis <jxd8037@g.rit.edu>
2018-04-08 13:13:32 -0400

    Worked on Game hud

---
+++ FindTheKind/Assets/Scripts/GameHUD/HudStates.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudStates : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] panels;
+
+    public void ShowHud()
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(true);
+        }
+    }
+}
